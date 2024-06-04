2859de706e7e6930b5f99f1133ec00b26560b156
SEONG IK HAN <tjddlr3512@pusan.ac.kr>
2024-06-04 20:39:54 +0900

    Revert "Feat: 줄간격 수정 및 주석추가"

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,16 +1,15 @@
-
-// 3의 배수인 경우 FIZZ , 5의 배수일땐 BUZZ , 3과 5의 공배수일땐 FIZZBUZZ를 출력하는 함수
+//fizzBuzz 기능 구현
 function fizzBuzz(num) {
-  if (num % 3 === 0 && num % 5 === 0) {
-    return "FIZZBUZZ";
-  }
-  if (num % 3 === 0) {
-    return "FIZZ";
-  }
-  if (num % 5 === 0) {
-    return "BUZZ";
-  }
-  return num;
+    if (num % 3 === 0 && num % 5 === 0) {
+        return 'FIZZBUZZ';
+    }
+    if (num % 3 === 0) {
+        return 'FIZZ';
+    }
+    if (num % 5 === 0) {
+        return 'BUZZ';
+    }
+    return num;
 }
 
 for (let i = 1; i <= 20; i++) {
