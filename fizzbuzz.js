408231f7f4a6644fd020b26ea1af107994623df0
JaeAnHan <jaean1999@naver.com>
2024-06-04 19:44:47 +0900

    feat: add fizzbuzz

---
+++ fizzbuzz.js
@@ -0,0 +1,12 @@
+function fizzBuzz(num) {
+    if (num % 3 === 0 && num % 5 === 0) {
+        return 'FIZZBUZZ';
+    }
+    if (num % 3 === 0) {
+        return 'FIZZ';
+    }
+    if (num % 5 === 0) {
+        return 'BUZZ';
+    }
+    return num;
+}
