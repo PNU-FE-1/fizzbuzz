6a7e44f11e5b4cd017e04a20788c71dd2638f57c
seongikx <tjddlr3512@pusan.ac.kr>
2024-06-04 20:08:10 +0900

    Feat: 줄간격 수정 및 주석추가

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,14 +1,16 @@
+// 3의 배수인 경우 FIZZ , 5의 배수일땐 BUZZ , 3과 5의 공배수일땐 FIZZBUZZ를 출력하는 함수
+
 function fizzBuzz(num) {
-    if (num % 3 === 0 && num % 5 === 0) {
-        return 'FIZZBUZZ';
-    }
-    if (num % 3 === 0) {
-        return 'FIZZ';
-    }
-    if (num % 5 === 0) {
-        return 'BUZZ';
-    }
-    return num;
+  if (num % 3 === 0 && num % 5 === 0) {
+    return "FIZZBUZZ";
+  }
+  if (num % 3 === 0) {
+    return "FIZZ";
+  }
+  if (num % 5 === 0) {
+    return "BUZZ";
+  }
+  return num;
 }
 
 fizzBuzz(10);
