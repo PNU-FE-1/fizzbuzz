3117ee46f74a28d4a942e727d9bd50660805dfc1
Eric <real.dassy@gmail.com>
2024-06-04 20:44:33 +0900

    feat: Conflict 유발

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,14 +1,23 @@
 function fizzBuzz(num) {
   if (num % 3 === 0 && num % 5 === 0) {
+    console.log("CONFLICT!");
     return "FIZZBUZZ";
+    console.log("CONFLICT!");
   }
   if (num % 3 === 0) {
+    console.log("CONFLICT!");
     return "FIZZ";
+    console.log("CONFLICT!");
   }
+  console.log("CONFLICT!");
   if (num % 5 === 0) {
+    console.log("CONFLICT!");
     return "BUZZ";
+    console.log("CONFLICT!");
   }
+  console.log("CONFLICT!");
   return num;
+  console.log("CONFLICT!");
 }
 
 for (let i = 1; i <= 21; i++) {
